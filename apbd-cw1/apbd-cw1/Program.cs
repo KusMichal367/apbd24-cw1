1814f92fa6bf094fc73d18b4ef163b2fd5e887ee
Michał Kuś <s30367@pjwstk.edu.pl>
2025-03-04 15:08:14 +0100

    Added GetAverage()

--- apbd-cw1/apbd-cw1/Program.cs
+++ apbd-cw1/apbd-cw1/Program.cs
@@ -2,3 +2,13 @@
 Console.WriteLine("Commit 1");
 Console.WriteLine("Commit 2");
 Console.WriteLine("Commit 3");
+
+static double GetAverage(int[] arr)
+{
+    double sum = 0;
+    for (int i = 0; i<arr.Length; i++) {  sum += arr[i]; }
+    return sum/arr.Length;
+}
+
+int[] arr = { 1, 2, 3, 4, 5 };
+Console.WriteLine(GetAverage(arr));
